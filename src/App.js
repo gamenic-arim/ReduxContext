33bb17a8a74ee5d6ea8be6b7e50a0879bd392f72
gamenic-arim <gamenic.arim@gmail.com>
2019-06-02 22:03:36 +0545

    reduxcontext

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MyContext = React.createContext();
+
+class MyProvider extends Component {
+  state = {
+    name: "arim shrestha",
+    age: 23,
+    address: "dhapashi"
+  };
+
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          birthday: () =>
+            this.setState({
+              age: this.state.age + 1
+            })
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+class Person extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        So How do i access my file here ??
+        <MyContext.Consumer>
+          {context => (
+            <>
+              <p>My name is {context.state.name} </p>
+              <p>My age is {context.state.age} </p>
+              <p>My address is {context.state.address} </p>
+
+              <button onClick={context.birthday}>Happy birthday</button>
+            </>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
+
+class Family extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        hello is from family
+        <MyContext.Consumer>
+          {
+            (context) =>(
+              <>
+                i need to access props in family
+                <p>
+                  {context.state.name}
+                </p>
+              </>
+              )
+            }
+        </MyContext.Consumer>
+        <Person />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <Family />
+      </MyProvider>
+    );
+  }
 }
 
 export default App;
